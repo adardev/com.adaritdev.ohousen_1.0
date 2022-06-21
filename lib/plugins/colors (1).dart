95ff70ff14aa90816fdcf3aeb1c4309cc68c9f4d
adaritdev <adaritdevbusiness@gmail.com>
2022-06-21 10:17:37 -0500

    update

--- lib/plugins/colors (1).dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kPrimaryColorLight = Color(0xFFFFFFFF);
-const kPrimaryColorNight = Color(0xFF000000);
-
-const kIconColorsLight = Color(0xFF000000);
-const kIconColorsNight = Color(0xFFFFFFFF);
-
-
